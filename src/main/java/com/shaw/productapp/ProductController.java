1fc9990e42b944f1b91c11d5a2b7d118b8f68e64
animireddy-san <67856624+animireddy-san@users.noreply.github.com>
2025-08-15 23:33:25 +0530

    Update ProductController.java

--- src/main/java/com/shaw/productapp/ProductController.java
+++ src/main/java/com/shaw/productapp/ProductController.java
@@ -11,6 +11,6 @@ public class ProductController {
     @GetMapping("/products")
     public String getProducts()
     {
-        return "Welcome to Apple Products";
+        return "Welcome to Ornge Products";
     }
 }
